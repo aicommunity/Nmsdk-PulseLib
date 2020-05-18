fcdee0f7600aaa1a783b32d2460402ff9ab0c15f
Alexab <palexab@gmail.com>
2020-05-18 11:06:50 +0300

    STDP synapse model has been added.

---
+++ Core/NPulseSynapseStdp.cpp
@@ -0,0 +1,213 @@
+// ===========================================================
+// Version:        3.0.2
+// ===========================================================
+/* ***********************************************************
+@Copyright Alexander V. Bakhshiev, 2020.
+E-mail:		alexab@ailab.ru
+url:            http://ailab.ru
+
+This file - part of the project: Neuro Modeler Software Developer Kit (NMSDK)
+
+File License:        BSD License
+Project License:     BSD License
+See file license.txt for more information
+*********************************************************** */
+
+#ifndef NPULSE_SYNAPSE_STDP_CPP
+#define NPULSE_SYNAPSE_STDP_CPP
+
+#include "NPulseSynapseStdp.h"
+#include "NPulseNeuron.h"
+#include "NPulseChannel.h"
+
+namespace NMSDK {
+
+// Методы
+// --------------------------
+// Конструкторы и деструкторы
+// --------------------------
+NPulseSynapseStdp::NPulseSynapseStdp(void)
+ //: NConnector(name),
+: APlus("APlus",this,&NPulseSynapseStdp::SetAPlus),
+  AMinus("AMinus",this,&NPulseSynapseStdp::SetAMinus),
+  XTau("XTau",this,&NPulseSynapseStdp::SetXTau),
+  YTau("YTau",this,&NPulseSynapseStdp::SetYTau),
+  XAvg("XAvg",this),
+  YAvg("YAvg",this),
+  XYDiff("XYDiff",this),
+  StdpInfluence("StdpInfluence",this)
+ // Input("Input",this)
+{
+}
+
+NPulseSynapseStdp::~NPulseSynapseStdp(void)
+{
+}
+// --------------------------
+
+
+// --------------------------
+// Методы управления общедоступными свойствами
+// --------------------------
+// Постоянная времени выделения медиатора
+bool NPulseSynapseStdp::SetAPlus(const double &value)
+{
+ if(value <= 0)
+  return false;
+
+ return true;
+}
+
+// Постоянная времени распада медиатора
+bool NPulseSynapseStdp::SetAMinus(const double &value)
+{
+ if(value <= 0)
+  return false;
+
+ return true;
+}
+
+// Коэффициент пресинаптического торможения
+bool NPulseSynapseStdp::SetXTau(const double &value)
+{
+ if(value <= 0)
+  return false;
+
+ return true;
+}
+
+// Коэффициент пресинаптического торможения
+bool NPulseSynapseStdp::SetYTau(const double &value)
+{
+ if(value <= 0)
+  return false;
+
+ return true;
+}
+// --------------------------
+
+
+// --------------------------
+// Системные методы управления объектом
+// --------------------------
+// Выделяет память для новой чистой копии объекта этого класса
+NPulseSynapseStdp* NPulseSynapseStdp::New(void)
+{
+ return new NPulseSynapseStdp;
+}
+// --------------------------
+
+// --------------------------
+// Скрытые методы управления счетом
+// --------------------------
+// Восстановление настроек по умолчанию и сброс процесса счета
+bool NPulseSynapseStdp::ADefault(void)
+{
+ if(!NPulseSynapse::ADefault())
+  return false;
+
+ APlus=1e13;
+ AMinus=1e13;
+ XTau=1e-2;
+ YTau=1e-3;
+ StdpInfluence.Assign(0,0,0.0);
+
+ return true;
+}
+
+// Обеспечивает сборку внутренней структуры объекта
+// после настройки параметров
+// Автоматически вызывает метод Reset() и выставляет Ready в true
+// в случае успешной сборки
+bool NPulseSynapseStdp::ABuild(void)
+{
+ if(!NPulseSynapse::ABuild())
+  return false;
+
+ return true;
+}
+
+// Сброс процесса счета.
+bool NPulseSynapseStdp::AReset(void)
+{
+ if(!NPulseSynapse::AReset())
+  return false;
+
+ XAvg=0;
+ YAvg=0;
+ XYDiff=0;
+ StdpInfluence.ToZero();
+ return true;
+}
+
+// Выполняет расчет этого объекта
+bool NPulseSynapseStdp::ACalculate(void)
+{
+ bool is_input_pulse_active(false);
+ bool is_output_pulse_active(false);
+
+ if(!Input.IsConnected() || Input->GetCols()<=0)
+  return true;
+
+ if(!MainOwner)
+  return true;
+
+ NPulseLTZone* zone=static_pointer_cast<NPulseNeuronCommon>(MainOwner)->LTZone;
+ if(!zone)
+  return true;
+
+ if(zone->Output->GetCols()<=0)
+  return true;
+
+ if((*Input)(0,0)>0)
+  is_input_pulse_active=true;
+
+ if(zone->Output(0,0)>0)
+  is_output_pulse_active=true;
+
+ if(is_output_pulse_active)
+  XAvg.v+=(Resistance.v-XAvg.v)/(XTau.v*TimeStep);
+ else
+  XAvg.v-=XAvg.v/(XTau.v*TimeStep);
+
+ if(is_input_pulse_active)
+  YAvg.v+=(Resistance.v-YAvg.v)/(YTau.v*TimeStep);
+ else
+  YAvg.v-=YAvg.v/(YTau.v*TimeStep);
+
+ double x_avg_res=(is_output_pulse_active)?XAvg.v:0;
+ double y_avg_res=(is_input_pulse_active)?YAvg.v:0;;
+
+ XYDiff.v+=(x_avg_res-y_avg_res)/TimeStep;
+
+ StdpInfluence(0,0)=(1-XYDiff.v);
+ Output(0,0)*=StdpInfluence(0,0);
+  	  /*
+ if(Input.IsConnected() && Input->GetCols()>0)
+ {
+  input=(*Input)(0,0);
+  if(MainOwner && Owner)
+  {
+   if(static_pointer_cast<NPulseChannel>(Owner)->Type() < 0)
+	++static_pointer_cast<NPulseNeuron>(MainOwner)->NumActivePosInputs.v;
+   else
+	++static_pointer_cast<NPulseNeuron>(MainOwner)->NumActiveNegInputs.v;
+  }
+ }
+
+ if(input>0)
+  PreOutput.v+=(input/PulseAmplitude.v-PreOutput.v)/VSecretionTC;
+ else
+  PreOutput.v-=PreOutput.v/VDissociationTC;
+
+ Output(0,0)=OutputConstData*
+						(1.0-InhibitionCoeff.v*PreOutput.v)*PreOutput.v;
+
+ if(Output(0,0)<0)
+  Output(0,0)=0;
+				   */
+ return true;
+}
+// --------------------------
+}
+#endif
---
+++ Core/NPulseSynapseStdp.h
@@ -0,0 +1,108 @@
+// ===========================================================
+// Version:        3.0.2
+// ===========================================================
+/* ***********************************************************
+@Copyright Alexander V. Bakhshiev, 2020.
+E-mail:		alexab@ailab.ru
+url:            http://ailab.ru
+
+This file - part of the project: Neuro Modeler Software Developer Kit (NMSDK)
+
+File License:        BSD License
+Project License:     BSD License
+See file license.txt for more information
+*********************************************************** */
+
+#ifndef NPULSE_SYNAPSE_STDP_H
+#define NPULSE_SYNAPSE_STDP_H
+
+#include "NPulseSynapse.h"
+
+
+namespace NMSDK {
+
+class RDK_LIB_TYPE NPulseSynapseStdp: public NPulseSynapse
+{
+public: // Общедоступные свойства
+ULProperty<double,NPulseSynapseStdp, ptPubParameter> APlus;
+
+ULProperty<double,NPulseSynapseStdp, ptPubParameter> AMinus;
+
+ULProperty<double,NPulseSynapseStdp, ptPubParameter> XTau;
+ULProperty<double,NPulseSynapseStdp, ptPubParameter> YTau;
+
+public: // Переменные состояния
+/// Усреденение входного импульса
+ULProperty<double,NPulseSynapseStdp, ptPubState> XAvg;
+
+/// Усреденение выходного импульса
+ULProperty<double,NPulseSynapseStdp, ptPubState> YAvg;
+
+/// Промежуточная разность влияния X и Y компонент
+ULProperty<double,NPulseSynapseStdp, ptPubState> XYDiff;
+
+
+public: // Входы и выходы
+/// Выходной сигнал влияния STDP
+UPropertyOutputData<MDMatrix<double>, NPulseSynapseStdp, ptOutput | ptPubState> StdpInfluence;
+
+protected: // Основные свойства
+
+protected: // Временные переменные
+
+public: // Методы
+// --------------------------
+// Конструкторы и деструкторы
+// --------------------------
+NPulseSynapseStdp(void);
+virtual ~NPulseSynapseStdp(void);
+// --------------------------
+
+protected:
+// --------------------------
+// Методы управления общедоступными свойствами
+// --------------------------
+// Устанавливает амплитуду импульсов
+bool SetAPlus(const double &value);
+
+// Постоянная времени выделения медиатора
+bool SetAMinus(const double &value);
+
+// Постоянная времени распада медиатора
+bool SetXTau(const double &value);
+
+// Постоянная времени распада медиатора
+bool SetYTau(const double &value);
+// --------------------------
+
+public:
+// --------------------------
+// Системные методы управления объектом
+// --------------------------
+// Выделяет память для новой чистой копии объекта этого класса
+virtual NPulseSynapseStdp* New(void);
+// --------------------------
+
+// --------------------------
+// Скрытые методы управления счетом
+// --------------------------
+protected:
+// Восстановление настроек по умолчанию и сброс процесса счета
+virtual bool ADefault(void);
+
+// Обеспечивает сборку внутренней структуры объекта
+// после настройки параметров
+// Автоматически вызывает метод Reset() и выставляет Ready в true
+// в случае успешной сборки
+virtual bool ABuild(void);
+
+// Сброс процесса счета.
+virtual bool AReset(void);
+
+// Выполняет расчет этого объекта
+virtual bool ACalculate(void);
+// --------------------------
+};
+
+}
+#endif
