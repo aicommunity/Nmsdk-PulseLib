a8dafdd7022d73495fb661cd20c1ab6638f6a2cd
AlexandraDemcheva <ademtcheva@gmail.com>
2025-01-18 19:17:51 +0300

    Added NeuronTrainer for MazeMemory
    Added new version of NeuronTrainer (IsNeedToTrain=false by default)

--- Core/NNeuronTrainer.h
+++ Core/NNeuronTrainer.h
@@ -59,6 +59,9 @@ ULProperty<std::string, NNeuronTrainer, ptPubParameter> SynapseClassName;
 /// Признак необходимости обучения
 ULProperty<bool, NNeuronTrainer, ptPubParameter> IsNeedToTrain;
 
+/// Флаг режима сборки внутри MazeMemroy
+ULProperty<bool, NNeuronTrainer, ptPubParameter> IsUsedInMemory;
+
 /// Время задержки начала обучения относительно старта системы (сек)
 ULProperty<double, NNeuronTrainer, ptPubParameter> Delay;
 
@@ -99,6 +102,7 @@ UPropertyOutputData<MDMatrix<double>, NNeuronTrainer, ptOutput | ptPubState> Out
 
 /// Сопротивление нарастающих синапсов
 ULProperty<double, NNeuronTrainer, ptPubParameter> SynapseResistanceStep;
+
 
 protected:
 
@@ -206,6 +210,9 @@ bool SetInputPattern(const MDMatrix<double> &value);
 /// Признак необходимости обучения
 bool SetNeedToTrain(const bool &value);
 
+/// Флаг режима сборки внутри MazeMemroy
+bool SetIsUsedInMemory(const bool &value);
+
 /// Порог низкопороговой зоны нейрона
 bool SetLTZThreshold(const double &value);
 
@@ -220,6 +227,7 @@ bool SetUseFixedLTZThreshold(const bool &value);
 
 /// Сопротивление нарастающих синапсов
 bool SetSynapseResistanceStep(const double &value);
+
 
 // --------------------------
 
